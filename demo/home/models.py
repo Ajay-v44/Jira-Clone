72ed3f710c4222399b53c45c187e30d630010adb
Ajay v <vajay4834@gmail.com>
2023-10-05 11:29:52 +0530

    tables cretaed

--- demo/home/models.py
+++ demo/home/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Tasks(models.Model):
+    task_description=models.CharField(max_length=100)
+    status=models.CharField(default="pending",max_length=50)
