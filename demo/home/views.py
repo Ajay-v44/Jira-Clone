b536d6446c15d841a3936f7ba777b6817ea69ff9
Ajay v <vajay4834@gmail.com>
2023-10-05 18:11:44 +0530

    read operation finished

--- demo/home/views.py
+++ demo/home/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import *
+
+
 # Create your views here.
 def home(request):
-    return render(request,"index.html")
+    if request.method =="POST":
+        data=request.POST
+        task=data.get('task')
+        Tasks.objects.create(
+            task_description=task,
+        )
+        print(task)
+
+    return render(request,'index.html')
