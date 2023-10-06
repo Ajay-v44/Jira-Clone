b9808cd3d831a937d43adce2b1f84a5fb97f8519
Ajay v <vajay4834@gmail.com>
2023-10-06 11:23:24 +0530

    js updated

--- demo/home/views.py
+++ demo/home/views.py
@@ -1,35 +1,46 @@
 from contextlib import redirect_stderr
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from .models import *
+from django.http import JsonResponse
 
 
 # Create your views here.
 def home(request):
-    if request.method =="POST":
-        data=request.POST
-        task=data.get('task')
+    if request.method == "POST":
+        data = request.POST
+        task = data.get('task')
         Tasks.objects.create(
             task_description=task,
         )
         print(task)
-    queryset=Tasks.objects.all()
-   
-    context={'Tasks':queryset}
+    queryset = Tasks.objects.all()
 
-    return render(request,'index.html',context)
+    context = {'Tasks': queryset}
 
-def completed_tasks(request,id):
-    queryset=Tasks.objects.get(id=id)
-    queryset.status="completed"
+    return render(request, 'index.html', context)
+
+
+def completed_tasks(request, id):
+    queryset = Tasks.objects.get(id=id)
+    queryset.status = "completed"
     queryset.save()
     return redirect('home')
-def deleted_tasks(request,id):
-    queryset=Tasks.objects.get(id=id)
-    queryset.status="deleted"
-    queryset.save()
-    return redirect('home')
-def remove_tasks(request,id):
-    queryset=Tasks.objects.get(id=id)
-    queryset.status="removed"
+
+
+def deleted_tasks(request, id):
+    try:
+        queryset = Tasks.objects.get(id=id)
+        queryset.status = "deleted"
+
+        queryset.save()
+       
+        return redirect('home')
+    except Tasks.DoesNotExist:
+        return JsonResponse({'success': False, 'error_message': 'Task not found'})
+
+
+def remove_tasks(request, id):
+    queryset = Tasks.objects.get(id=id)
+    queryset.status = "removed"
     queryset.save()
     return redirect('home')
